34d81cb6745e8c4905115b4c2a2c398dc60ff994
Mike Stowe <SnoopSqueak@aim.com>
2019-05-25 10:38:26 -0500

    Basic Node, Express, Jasmine setup

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+const http = require("http");
+const server = http.createServer(app);
+
+const port = 3000;
+
+server.listen(port);
+
+server.on("listening", () => {
+  console.log("Server is listening for requests on port " + port);
+});
