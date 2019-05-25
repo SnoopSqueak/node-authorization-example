6e4203b3cc9c6582d199b9c2eb864066ccd31e3d
Mike Stowe <SnoopSqueak@aim.com>
2019-05-25 13:43:35 -0500

    Added basic users

---
+++ src/controllers/userController.js
@@ -0,0 +1,53 @@
+const userQueries = require("../db/queries.users.js");
+const passport = require("passport");
+
+module.exports = {
+  signUp(req, res, next) {
+    res.render("users/sign_up");
+  },
+
+  signInForm(req, res, next) {
+    res.render("users/sign_in");
+  },
+
+  signIn(req, res, next) {
+    passport.authenticate("local")(req, res, () => {
+      if (!req.user) {
+        req.flash("notice", "Sign in failed. Please try again.");
+        res.redirect("/users/sign_in");
+      } else {
+        req.flash("notice", "You've successfully signed in!");
+        res.redirect("/");
+      }
+    });
+  },
+
+  signOut(req, res, next) {
+    req.logout();
+    req.flash("notice", "You've successfully signed out!");
+    res.redirect("/");
+  },
+
+  create(req, res, next) {
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirmation: req.body.passwordConfirmation
+    };
+    userQueries.createUser(newUser, (err, user) => {
+      if (err) {
+        console.log(err);
+        // this line probably won't work based on messages.ejs...
+        // i'll check the error object in the log and figure out which
+        // property stores the actual error message
+        req.flash("error", {param: err.name, msg: err.errors[0].message});
+        res.redirect("/users/sign_up");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've successfully signed in!");
+          res.redirect("/");
+        });
+      }
+    });
+  }
+};
